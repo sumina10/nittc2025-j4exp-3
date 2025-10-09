5979b0eee80741b2098db3009d5512fceda83049
imosarumushi <s.09059585590@gmail.com>
2025-10-09 16:05:56 +0900

    クラスの対応

--- task/models.py
+++ task/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+class User(models.Model):
+    passWord= models.CharField(max_length=16)
+    
+
 class Course(models.Model):
     name = models.CharField(max_length=16)
 
@@ -9,7 +13,5 @@ class Assignment(models.Model):
     dueDate = models.DateTimeField(auto_now=False)
     status = models.CharField(max_length=8)
     course = models.ForeignKey(Course,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
 
-class User(models.Model):
-    passWord= models.CharField(max_length=16)
-    assigment = models.ForeignKey(Assignment,on_delete=models.CASCADE)
