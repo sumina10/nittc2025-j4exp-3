cfce63aaa3871de64fa86d73cba60b93ce755201
imosarumushi <s.09059585590@gmail.com>
2025-10-09 16:00:36 +0900

    しゅうせいしました。。

--- task/apps.py
+++ task/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PollsConfig(AppConfig):
+class taskConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'polls'
+    name = 'task'
