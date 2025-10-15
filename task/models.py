32e9c858684c1c249b9a0d215749790094476af8
TangentMochi <yu180609@gmail.com>
2025-10-15 15:53:28 +0900

    表記揺れを対処

--- task/models.py
+++ task/models.py
@@ -3,15 +3,15 @@ from django.conf import settings
 from accounts.models import Teacher, Student
 
 class ClassRoom(models.Model):
-    name = models.CharField(max_length=16)
+    title = models.CharField(max_length=16)
     # 教師は多対多
-    teacher = models.ManyToManyField(Teacher, related_name='classrooms_teachers')
+    teachers = models.ManyToManyField(Teacher, related_name='classrooms_teachers')
     # 生徒は拡張性を考慮して多対多
     students = models.ManyToManyField(Student, related_name='classrooms_students')
 
 class Course(models.Model):
-    name = models.CharField(max_length=16)
-    teacher = models.ManyToManyField(Teacher, related_name='courses_teachers')
+    title = models.CharField(max_length=16)
+    teachers = models.ManyToManyField(Teacher, related_name='courses_teachers')
 
 class Assignment(models.Model):
     title = models.CharField(max_length=16)
