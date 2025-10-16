30febdfb6baaad9d35c74511b7587958143766a7
imosarumushi <s.09059585590@gmail.com>
2025-10-16 15:11:50 +0900

    テンプレート作りました

--- task/views.py
+++ task/views.py
@@ -1,3 +1,9 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
+
+def template_test(request):
+    return render(
+        request,
+        "task/base.html"
+    )
