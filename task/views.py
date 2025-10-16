b0a8bc5de789afe946e1392298be67f42db13328
Ura2 <51593994+Uranium4581@users.noreply.github.com>
2025-10-16 13:42:19 +0900

    teketou.html

--- task/views.py
+++ task/views.py
@@ -1,3 +1,10 @@
-# from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from .models import Assignment
 
 # Create your views here.
+
+class Assignment(CreateView):
+    model = Assignment
+    fields = ["title","description"]
+    template_name = "templates/task/teketou.html"
