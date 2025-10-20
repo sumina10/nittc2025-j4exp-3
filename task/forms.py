17973e2d33bcce1f031f7ce379486c148ef08ede
TangentMochi <yu180609@gmail.com>
2025-10-21 07:22:37 +0900

    日時の入力ができるように

---
+++ task/forms.py
@@ -0,0 +1,19 @@
+from django.forms.widgets import DateTimeInput
+from django import forms
+
+from .models import Assignment, Course
+
+class AssignmentForm(forms.ModelForm):
+    class Meta:
+        model = Assignment
+        fields = ['title', 'description', 'due_date', 'course']
+        widgets = {
+            'due_date': DateTimeInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
+        }
+    
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)        
+        self.fields['course'].queryset = Course.objects.filter(
+            classroom__students=user
+        ).distinct()
--- task/views.py
+++ task/views.py
@@ -5,36 +5,35 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils import timezone
 
 from .models import Assignment
+from .forms import AssignmentForm
 
 # Create your views here.
 
 class CreateAssignment(LoginRequiredMixin, CreateView):
     model = Assignment
-    fields = ["title","description","course", "status","due_date"] # studentはログインしている人のIDを使うためあとで付け足し
+    form_class = AssignmentForm
     template_name = "task/registration.html"
 
     success_url = reverse_lazy('task-create') # 'task-create'はリダイレクト先のURLパターン名
 
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
     def form_valid(self, form):
-        # print("--- ログインした人の情報 ---\n")
-        # print(f"{self.request.user}\n") # ログインしている人の情報
         form.instance.student = self.request.user
-        form.instance.due_date = timezone.now()
-        
-        # print("--- フォームに入力されたデータ(結合前) ---\n")
-        # print(form.cleaned_data)
-        
-        print("--- フォームに入力されたデータ(結合後) ---\n")
+        # form.instance.due_date = timezone.now()
+    
         combined_data = form.cleaned_data.copy()
         combined_data['student'] = form.instance.student
-        combined_data['due_date'] = form .instance.due_date
-        print(combined_data)
+        # combined_data['due_date'] = form.instance.due_date
         return super().form_valid(form)
 
 class CreateStudentHome(LoginRequiredMixin,ListView): #CreateStudentHomeがListViewとLoginRequiredMixinを継承
-    model = Assignment #クラスを作る時点では全部の情報が取得できてOK
-    # queryset = Assignment.objects.filter(student = self.request.user)
+    model = Assignment
     template_name = "task/stu_home.html"
+
     def get_queryset(self):
         queryset = super().get_queryset() # ListViewに則ってget_querysetを実行
         return queryset.filter(student = self.request.user) # 返してくれた情報をもとに今ログインしている人の情報のみを返す
