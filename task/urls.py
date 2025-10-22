35063b71b82417227b85a6e9a7a301cd0129eec4
TangentMochi <yu180609@gmail.com>
2025-10-22 21:07:48 +0900

    stu-homeのリネーム

--- task/urls.py
+++ task/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateAssignment, StudentAssignmentView
+from .views import CreateAssignment, StudentAssignmentView, TeacherAssignmentView
 
 urlpatterns = [
     path('create/',CreateAssignment.as_view(),name='task-create'),
-    path('stu_home/',StudentAssignmentView.as_view(),name='stu-home')
+    path('student_home/',StudentAssignmentView.as_view(),name='student-home'),
+    path('teacher_home/',TeacherAssignmentView.as_view(),name='teacher-home'),
 ]
