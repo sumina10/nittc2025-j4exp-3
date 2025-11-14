d6b9f139d80030204b484a22dc044d32f9a682e1
TangentMochi <yu180609@gmail.com>
2025-11-14 21:36:24 +0900

    urlの問題を解決

--- nittc/urls.py
+++ nittc/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
